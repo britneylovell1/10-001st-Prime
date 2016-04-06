6228372ffc418f05c0b04a63f33ea4eed96023d7
Britney Lovell <britneyklovell@gmail.com>
2016-04-05 19:27:02 -0700

    Update 10001st_prime.py

--- 10001st_prime.py
+++ 10001st_prime.py
@@ -1,9 +1,3 @@
-'''
-By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
-the 6th prime is 13. What is the 10 001st prime number?
-'''
-
-
 def check_prime(n):
     '''
     Divide n by all integers less than n.
